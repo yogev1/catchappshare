22866f3ec6fce0bcd7c243b57129a72c7c3002b8
Yogev <finegoz@gmail.com>
2017-09-02 17:50:13 -0700

    added apple Association File

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  get ':id' => 'events#show'
+  get 'eventsLink/:id' => 'events#show' 
 
   resources :pages do 
     collection do
