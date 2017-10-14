3cd2ba7da5bb9d80533a239b841d2502230deaf7
Yogev <finegoz@gmail.com>
2017-10-14 14:23:40 -0700

    update

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
   def show
   	firstres = HTTParty.get("http://devapi.wearecatchapp.com/catchapp-server/api/unauth/login?username=logicsports@icloud.com&password=zzz123")
   	auth = firstres.body.inspect.split('\\')[11].gsub(/"/, '')
-  	@event = HTTParty.get("http://devapi.wearecatchapp.com/catchapp-server/api/event/opensearch?external_code=Xbkqsz")
+  	@event = HTTParty.get("http://devapi.wearecatchapp.com/catchapp-server/api/event/opensearch?external_code=LUPLCE")
     # , 
   		#       "headers": {
     #       "Authorization"=> "#{auth}"
